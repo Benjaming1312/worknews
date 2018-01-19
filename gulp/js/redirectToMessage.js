768d7cd1221894fca4934a055b46cf4cf774b740
Ben <ashitman1312@gmail.com>
2018-01-20 02:49:01 +0800

    新增會員應徵紀錄，我的收藏，訊息記錄(未完成)，打工報-我要應徵按鈕(未完成)

---
+++ gulp/js/redirectToMessage.js
@@ -0,0 +1,13 @@
+/**
+ * when url has # tag
+ * page will active to message tab
+ */ 
+$(function () {
+  if (window.location.href.indexOf('#') > 0 && window.location.href.split('#')[1] === 'resume05') {
+    $('.resume .nav-tabs li a').each(function () {
+      if ($(this).attr('href') === '#resume05') {
+        $(this).click()
+      }
+    })
+  }
+})
