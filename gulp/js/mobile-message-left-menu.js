332a5d0bc47128c73138cd1922a6a0900555999a
Ben <ashitman1312@gmail.com>
2018-01-20 15:55:13 +0800

    finish message page

---
+++ gulp/js/mobile-message-left-menu.js
@@ -0,0 +1,11 @@
+/**
+ * use for message left
+ */
+$(function () {
+  $('.person-menu  #togglePersonMenu').click(function () {
+    $('.person-menu').toggleClass('active')
+  })
+  $('.person-menu .close').click(function () {
+    $('.person-menu').removeClass('active')
+  })
+})
