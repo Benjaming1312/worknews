fd1288833bb144ea5dbad181323101ae6c9b2424
Ben <ashitman1312@gmail.com>
2018-01-20 16:42:30 +0800

    finish mini-resume page

--- gulp/js/mobile-message-left-menu.js
+++ gulp/js/mobile-message-left-menu.js
@@ -2,7 +2,7 @@
  * use for message left
  */
 $(function () {
-  $('.person-menu  #togglePersonMenu').click(function () {
+  $('.person-menu #togglePersonMenu').click(function () {
     $('.person-menu').toggleClass('active')
   })
   $('.person-menu .close').click(function () {
