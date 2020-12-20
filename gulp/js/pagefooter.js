63dc9c0ddf0b95a13f03c1d8c781b9d2b5cfd429
Ben <ashitman1312@gmail.com>
2020-12-20 19:15:33 +0800

    1220

--- gulp/js/pagefooter.js
+++ gulp/js/pagefooter.js
@@ -2,4 +2,10 @@ $(function () {
   if (window.location.href.indexOf('work.html') > 0 || window.location.href.indexOf('part') > 0) {
     $('footer.foot').addClass('work-foot')
   }
+
+  if (window.location.href.indexOf('Temporary') >= 0 && $('.worktabs').is('.worktabs') && $('.server >div >.row').children().length === 0) {
+    // No work theme
+    $('.server .row').append('<div class="col-xs-12"><p class="text-center" style="font-size: 18px;">目前尚無工作資訊</p></div>')
+    // $('.work-foot').attr('style', 'position: fixed; bottom: 0;')
+  }
 })
