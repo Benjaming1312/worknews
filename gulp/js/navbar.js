0446e448598cae994f9b5bca7bf55f866147263d
Ben <ashitman1312@gmail.com>
2020-12-23 23:52:24 +0800

    1223

--- gulp/js/navbar.js
+++ gulp/js/navbar.js
@@ -15,18 +15,21 @@ function appendNav () {
 }
 // focus nav link
 function focusNav () {
+	$('.menu .nav.navbar-nav .active').removeClass('active')
+	$('.menulist .active').removeClass('active')
+
   $('.menu .nav.navbar-nav a').each(function () {
 		var target = $(this).attr('href')
 		var url = new URL(target)
-		console.log('%c (／‵Д′)／~ ╧╧ url : ', 'padding: .25rem; font-size: 14px; background: #12bdba; color: #fff', url.pathname)
-    if (window.location.pathname === url.pathname) {
+    if (window.location.href.indexOf(url.pathname) >= 0) {
+			// console.log('%c (／‵Д′)／~ ╧╧ url : ', 'padding: .25rem; font-size: 14px; background: #12bdba; color: #fff', [url.pathname, window.location.pathname])
       $(this).addClass('active')
     }
   })
   $('.menulist a').each(function () {
 		var mobileTarget = $(this).attr('href')
 		var url = new URL(mobileTarget)
-    if (window.location.pathname === url.pathname) {
+    if (window.location.href.indexOf(url.pathname) >= 0) {
       $(this).addClass('active')
     }
   })
@@ -37,7 +40,8 @@ $(function () {
   $(window).on('resize', function () {
     appendNav()
     focusNav()
-  })
-  focusNav()
-
+	})
+	setTimeout(function() {
+		focusNav()
+	}, 100)
 })
